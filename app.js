ca206684f7d3b93b24a371942e93611d789fc2db
mgrinko <glukym@gmail.com>
2016-08-08 20:54:14 +0300

    tree.js

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var data = {
+  "Рыбы": {
+    "Форель": {},
+    "Щука": {}
+  },
+
+  "Деревья": {
+    "Хвойные": {
+      "Лиственница": {},
+      "Ель": {}
+    },
+    "Цветковые": {
+      "Берёза": {},
+      "Тополь": {
+        "Цветковые": {
+          "Берёза": {},
+          "Тополь": {}
+        }
+      }
+    }
+  }
+};
+
+
+
+
+new Tree(document.getElementById('container1'), data);
+new Tree(document.getElementById('container2'), data['Рыбы']);
+new Tree(document.getElementById('container3'), data['Деревья']);
