10eb289bf2568e14bb1a73396be00b6545d5cbe3
mgrinko <glukym@gmail.com>
2016-08-18 21:06:49 +0300

    template was added

--- js/phoneCatalogue.js
+++ js/phoneCatalogue.js
@@ -1,25 +1,15 @@
 'use strict';
 
-let phone = {
-  "age": 0,
-  "id": "motorola-xoom-with-wi-fi",
-  "imageUrl": "img/phones/motorola-xoom-with-wi-fi.0.jpg",
-  "name": "Motorola XOOM\u2122 with Wi-Fi",
-  "snippet": "The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the world's first tablet powered by Android 3.0 (Honeycomb)."
-};
-
-let template = `
-  <li class="thumbnail">
-    <a href="#!/phones/${phone.id}" class="thumb">
-      <img alt="${phone.name}" src="${phone.imageUrl}">
-    </a>
-    <a href="#!/phones/${phone.id}">${phone.name}</a>
-    <p>${phone.snippet}</p>
-  </li>
-`;
+let template = document.getElementById('phone-catalogue-template').innerHTML;
 
 class PhoneCatalogue {
   constructor(options) {
+    console.log(template);
+
+    this._compiledTemplate = _.template(template);
+
+    console.log(this._compiledTemplate);
+
     this._el = options.element;
 
     this._render(options.phones);
@@ -29,7 +19,7 @@ class PhoneCatalogue {
         return;
       }
 
-      event.preventDefault();
+      //event.preventDefault();
 
       let phoneContainer = event.target.closest('[data-element="phone"]');
 
@@ -46,22 +36,8 @@ class PhoneCatalogue {
   }
 
   _render(phones) {
-    let html = '<ul class="phones">';
-
-    phones.forEach(phone => {
-      html += `
-        <li class="thumbnail" data-element="phone" data-phone-id="${phone.id}">
-          <a href="#!/phones/${phone.id}" class="thumb" data-element="phoneLink" >
-            <img alt="${phone.name}" src="${phone.imageUrl}">
-          </a>
-          <a href="#!/phones/${phone.id}" data-element="phoneLink">${phone.name}</a>
-          <p>${phone.snippet}</p>
-        </li>
-      `;
+    this._el.innerHTML = this._compiledTemplate({
+      phones: phones
     });
-
-    html += '</ul>';
-
-    this._el.innerHTML = html;
   }
 }
